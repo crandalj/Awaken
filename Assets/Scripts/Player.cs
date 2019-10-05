015a88d26dec515a85e39831faa1ba2026ceb4ba
Joseph Crandal <crandalj@gmail.com>
2019-10-05 15:31:50 -0400

    adding base scripts & player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private Health health;
+    private Stats stats;
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        stats = gameObject.GetComponent<Stats>();
+        health = gameObject.GetComponent<Health>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,24 @@
+﻿public class Stats
+{
+    public int ATTACK;
+    public int HEALTH;
+    public int MAX_HEALTH;
+    public int SPEED;
+
+    public Stats(int attack, int health, int speed)
+    {
+        this.ATTACK = attack;
+        this.HEALTH = health;
+        this.MAX_HEALTH = this.HEALTH;
+        this.SPEED = speed;
+    }
+
+    public Stats()
+    {
+        this.ATTACK = 1;
+        this.HEALTH = 1;
+        this.MAX_HEALTH = this.HEALTH;
+        this.SPEED = 1;
+    }
+         
+}
